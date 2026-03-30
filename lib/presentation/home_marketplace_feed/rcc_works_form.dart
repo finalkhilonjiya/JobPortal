9c138ab1ebd79a764ddb72d42d19c698d91c1e58
finalkhilonjiya <finalkhilonjiya@gmail.com>
2026-03-30 11:15:48 +0530

    Update rcc_works_form.dart

--- lib/presentation/home_marketplace_feed/rcc_works_form.dart
+++ lib/presentation/home_marketplace_feed/rcc_works_form.dart
@@ -318,8 +318,8 @@ Padding(
         ),
         title: const Text("Request Submitted"),
         content: const Text(
-          "Your RCC construction request has been submitted successfully. Our team will contact you shortly.",
-        ),
+  "Your RCC construction request has been submitted successfully. Khilonjiya Support Team will contact you shortly.",
+),
         actions: [
           TextButton(
             onPressed: () => Navigator.pop(context),
