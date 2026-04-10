6bf3ee0ebef4b7767b2264e9e1d298e7fd1aff54
finalkhilonjiya <finalkhilonjiya@gmail.com>
2026-04-10 12:32:36 +0530

    Update gradlew.bat

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 SET DIR=%~dp0
-java -jar "%DIR%\gradle\wrapper\gradle-wrapper.jar" %*
+SET CLASSPATH=%DIR%\gradle\wrapper\gradle-wrapper.jar
+java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
