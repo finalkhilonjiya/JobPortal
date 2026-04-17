812460119d1747cbc2b3b9b7986af9c40a71954d
finalkhilonjiya <finalkhilonjiya@gmail.com>
2026-04-17 18:24:16 +0530

    Update employer_login_screen.dart

--- lib/presentation/auth/employer_login_screen.dart
+++ lib/presentation/auth/employer_login_screen.dart
@@ -6,7 +6,6 @@ import '../../routes/app_routes.dart';
 import '../../core/auth/user_role.dart';
 import '../../services/mobile_auth_service.dart';
 import 'package:sms_autofill/sms_autofill.dart';
-import '../../services/employer_service.dart';
 
 class EmployerLoginScreen extends StatefulWidget {
   const EmployerLoginScreen({Key? key}) : super(key: key);
@@ -18,7 +17,6 @@ class EmployerLoginScreen extends StatefulWidget {
 class _EmployerLoginScreenState extends State<EmployerLoginScreen>
     with SingleTickerProviderStateMixin, CodeAutoFill {
   final _mobileController = TextEditingController();
-final EmployerService _employerService = EmployerService();
 
   final List<TextEditingController> _otpControllers =
       List.generate(6, (_) => TextEditingController());
@@ -179,15 +177,10 @@ void codeUpdated() {
       role: UserRole.employer,
     );
 
-    final isMember = await _employerService.isUserInCompany();
-
     if (!mounted) return;
 
-    if (isMember) {
-      Navigator.pushReplacementNamed(context, AppRoutes.home);
-    } else {
-      Navigator.pushReplacementNamed(context, AppRoutes.createOrganization);
-    }
+    // ✅ ALWAYS GO TO HOME
+    Navigator.pushReplacementNamed(context, AppRoutes.home);
 
   } catch (e) {
     setState(() {
