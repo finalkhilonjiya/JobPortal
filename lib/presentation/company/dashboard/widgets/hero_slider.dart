0594686bcb5326aebbdc505d76f1b969d2e76fb0
finalkhilonjiya <finalkhilonjiya@gmail.com>
2026-04-20 14:44:08 +0530

    Update hero_slider.dart

--- lib/presentation/company/dashboard/widgets/hero_slider.dart
+++ lib/presentation/company/dashboard/widgets/hero_slider.dart
@@ -1,1 +1,55 @@
-avav
+import 'package:flutter/material.dart';
+
+class HeroSlider extends StatelessWidget {
+  const HeroSlider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final data = [
+      ("Post jobs faster", "Reach candidates instantly"),
+      ("Track applicants", "Manage hiring pipeline"),
+      ("Schedule interviews", "Stay organized"),
+    ];
+
+    return SizedBox(
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: data.length,
+        itemBuilder: (_, i) {
+          return Container(
+            width: 260,
+            margin: const EdgeInsets.only(right: 12),
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: const Color(0xFF16A34A),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  data[i].$1,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 15,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  data[i].$2,
+                  style: const TextStyle(
+                    color: Colors.white70,
+                    fontSize: 12.5,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
