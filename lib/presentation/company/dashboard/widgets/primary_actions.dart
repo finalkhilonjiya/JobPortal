0d6a3a8c4f8a7a68c928a14a17e3400fa342bae0
finalkhilonjiya <finalkhilonjiya@gmail.com>
2026-04-25 14:22:58 +0530

    Update primary_actions.dart

--- lib/presentation/company/dashboard/widgets/primary_actions.dart
+++ lib/presentation/company/dashboard/widgets/primary_actions.dart
@@ -9,10 +9,9 @@ class PrimaryActions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final items = [
-      (Icons.add, "Post Job", AppRoutes.createJob),
-      (Icons.work_outline, "Jobs", AppRoutes.employerJobs),
-      (Icons.people_outline, "Applicants", AppRoutes.employerJobs),
-      
+      (Icons.add, "Post Job", "create"),
+      (Icons.work_outline, "Jobs", "jobs"),
+      (Icons.people_outline, "Applicants", "applicants"),
     ];
 
     return SizedBox(
@@ -21,30 +20,61 @@ class PrimaryActions extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: items.length,
         itemBuilder: (_, i) {
-          return GestureDetector(
-            onTap: () => Navigator.pushNamed(context, items[i].$3),
-            child: Container(
-              width: 90,
-              margin: const EdgeInsets.only(right: 12),
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              decoration: BoxDecoration(
-                color: Colors.white,
+          return Container(
+            width: 90,
+            margin: const EdgeInsets.only(right: 12),
+            child: Material(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(14),
+              child: InkWell(
                 borderRadius: BorderRadius.circular(14),
-                border: Border.all(color: const Color(0xFFE6E8EC)),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(items[i].$1, size: 22),
-                  const SizedBox(height: 6),
-                  Text(
-                    items[i].$2,
-                    style: const TextStyle(
-                      fontSize: 12,
-                      color: Color(0xFF6B7280),
-                    ),
+                splashColor: const Color(0xFFDCFCE7),
+                highlightColor: Colors.transparent,
+                onTap: () {
+                  final type = items[i].$3;
+
+                  if (type == "create") {
+                    Navigator.pushNamed(context, AppRoutes.createJob);
+                  } else if (type == "jobs") {
+                    Navigator.pushNamed(context, AppRoutes.employerJobs);
+                  } else if (type == "applicants") {
+                    Navigator.pushNamed(
+                      context,
+                      AppRoutes.jobApplicants,
+                      arguments: {
+                        'jobId': 'all',
+                        'companyId': companyId,
+                      },
+                    );
+                  }
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    border: Border.all(color: const Color(0xFFE6E8EC)),
                   ),
-                ],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        items[i].$1,
+                        size: 22,
+                        color: const Color(0xFF16A34A),
+                      ),
+                      const SizedBox(height: 6),
+                      Text(
+                        items[i].$2,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Color(0xFF6B7280),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           );
